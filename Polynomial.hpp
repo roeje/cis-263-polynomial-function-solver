6b6c40bcbcab5a628615153da7f8210630b356b7
Jesse Roe <jessedroe@gmail.com>
2015-11-03 14:46:09 -0500

    Initial commit

---
+++ Polynomial.hpp
@@ -0,0 +1,166 @@
+/*
+ * Polynomial.hpp
+ *
+ *      Author: "Hans Dulimarta <dulimarh@cis.gvsu.edu>"
+ */
+
+#ifndef POLYNOM_H_
+#define POLYNOM_H_
+
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <climits>
+#include <math.h>
+
+#include <unordered_map>   // we use a HASH MAP
+
+using namespace std;
+
+template<typename T>
+class Polynomial {
+private:
+    /* coeffient and exponent of the polynom are stored as a pair<> */
+    vector<pair<T,int>> poly;
+
+public:
+    /* each term is a pair of coefficient and exponent, the type of
+     * coefficient is determined by <T>, but exponent is always an integer */
+
+    Polynomial() {
+    }
+
+    Polynomial(const string& input) {
+        load(input);
+    }
+
+    Polynomial(const Polynomial& other) {
+        poly = other.poly;
+    }
+
+    ~Polynomial()
+    {
+        poly.clear();
+    }
+
+    Polynomial& operator= (const Polynomial& other) {
+        poly = other.poly;
+        return *this;
+    }
+
+    /* TODO: complete this function to multiply "this" polynomial (of M
+     * terms) with the "other" polynomial (of N terms). Use the first 
+     * technique described in question 5.13: (a) store the MN terms of the
+     * product (b) sort them (c) combine like terms
+     */
+    Polynomial operator* (const Polynomial& other) const {
+        Polynomial result;
+        for(auto it = this->poly.begin(); it != poly.end(); ++it ){
+            for(auto it2 = other.poly.begin(); it2 != poly.end(); ++it2) {
+                auto a = it->first * it2->first;
+                auto b = it->second * it2->second;
+
+                for(auto it3 = result.poly.begin(); it3 != result.poly.end(); ++it3) {
+                    
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /* TODO: complete this function to multiply "this" polynomial (of M
+     * terms) with the "other" polynomial (of N terms). Use the alternate 
+     * technique described in question 5.13. Hint, use a map (or
+     * unordered_map) to merge like terms as they are computed.
+     */
+    Polynomial operator% (const Polynomial& other) const {
+        Polynomial result;
+        /* TODO: write your algorithm here */
+
+        return result;
+    }
+
+    /* Return the highest degree in the polynomial */
+    int maxDegree() const {
+        /* TODO: return the exponent of the first term (1 line of code) */
+        return 0;
+    }
+
+    /* return the k-th exponent or zero when the polynom has no terms */
+    int operator% (int k) {
+        /* TODO: write your code here (around 3-4 lines of code) */
+        return 0;
+    }
+
+    /* return the k-th coefficient, or zero when the polynom is empty */
+    T operator[] (int k) const {
+        /* TODO: write your code here (around 3-4 lines of code) */
+        return 0;
+    }
+
+    /* TODO evaluate the polynom for the given value */
+    T operator() (T arg) const {
+        return 0;
+    }
+
+    /* The following function "object" is needed for sorting
+     * the polynomial terms in descending order of the exponent */
+    struct exponent_comparator {
+        bool operator() (const pair<T,int>& a, const pair<T,int>& b) {
+            return a.second > b.second;
+        }
+    };
+
+private:
+    /* The load function reads in a string representation of a polynomial
+     * and creates a vector of "polynomial terms".
+     * The input string has the following format:
+     *
+     * [coeff int] [coeff int] .....
+     *
+     * For instance,   3x^5 - 7x^2 + 11 can be represented as one of 
+     * the following string (whitespaces do not matter)
+     *
+     * [3 5] [-7 2] [11 0]
+     * [3 5]   [-7   2]  [11 0]
+     */
+    void load(const string& polystring) {
+        /* use a string input stream */
+        stringstream input (polystring);
+        const int L = polystring.length();
+        T coeff;
+        int expo = INT_MIN, last_expo;
+        bool sortNeeded = false;
+
+        /* skip the input, upto and including the '[' */
+        input.ignore(L, '[');
+        last_expo = expo;
+
+        /* our string input stream is like a file, so we can check for
+         * "end-of-file".... */
+        while (!input.eof()) {
+            input >> coeff >> expo;
+            input.ignore(L, ']');
+
+            if (fabs(coeff) > 1e-6)  /* include only non-zero coeffs */
+            {
+                poly.push_back(make_pair(coeff,expo));
+                if (expo > last_expo)
+                    sortNeeded = true;
+                last_expo = expo;
+            }
+            input.ignore(L, '[');
+        }
+
+        /* sort the terms in increasing order of exponents */
+        if (sortNeeded)
+            sort(poly.begin(), poly.end(), exponent_comparator());
+    }
+};
+
+#endif /* POLYNOM_H_ */
---
+++ hw513_main.cpp
@@ -0,0 +1,149 @@
+#include "gvsu_cis.h"
+
+#include "Polynomial.hpp"
+#include <sstream>
+
+TEST_CASE ("Getter")
+{
+    /* 8x^100 - 3x^5 + 9 */
+    Polynomial<float> one("[8 100] [-3 5] [9 0]");
+
+    SECTION ("Coefficient getters") {
+        REQUIRE (one[0] == Approx(8.0));
+        REQUIRE (one[1] == Approx(-3.0));
+        REQUIRE (one[2] == Approx(9.0));
+    }
+    SECTION ("Exponent getters") {
+        REQUIRE (one % 0 == 100);
+        REQUIRE (one % 1 == 5);
+        REQUIRE (one % 2 == 0);
+    }
+
+    SECTION ("Highest Order") {
+        REQUIRE (one.maxDegree() == 100);
+    }
+}
+
+TEST_CASE ("Evaluate Constant Polynom")
+{
+    Polynomial<float> one("[2.5 0]");
+
+    for (int k = -10; k < 10; k++)
+        REQUIRE (one(k) == Approx(2.5));
+}
+
+TEST_CASE ("Evaluate Polynom")
+{
+    /* 4x^8 - 6x^3 - 9 */
+    Polynomial<float> one("[4 8] [-6 3] [-9 0]");
+
+    REQUIRE (one(-1) == Approx (1.0));
+    REQUIRE (one(0) == Approx (-9.0));
+    REQUIRE (one(1) == Approx (-11.0));
+    REQUIRE (one(2) == Approx (967));
+}
+
+TEST_CASE ("Multiplication")
+{
+    /* 2x^3 - 5x + 7 */
+    Polynomial<float> one("[2 3] [-5 1] [7 0]");
+
+    /* 6x^5 + x^2 */
+    Polynomial<float> two("[6 5] [1 2]");
+
+    Polynomial<float> result;
+
+    SECTION ("Multiplication by zero") {
+        Polynomial<float> zero("[0 0]");
+        result = one * zero;
+        REQUIRE (result.maxDegree() == 0);
+        REQUIRE (result[0] == Approx(0.0));
+        REQUIRE (result%0 == 0);
+    }
+
+    SECTION ("Multiplication by constant") {
+        Polynomial<float> constant ("[4 0]");
+        /* 2x^3 - 5x + 7 */
+        result = one * constant;
+        REQUIRE (result.maxDegree() == 3);
+        REQUIRE (result[0] == Approx(8.0));
+        REQUIRE (result[1] == Approx(-20.0));
+        REQUIRE (result[2] == Approx(28.0));
+        REQUIRE (result%0 == 3);
+        REQUIRE (result%1 == 1);
+        REQUIRE (result%2 == 0);
+    }
+
+    SECTION ("Self multiply") {
+        result = one * one;
+        //Polynomial<float> one("[2 3] [-5 1] [7 0]");
+        //[4 6][-20 4][28 3][25 2][-70 1][49 0]
+        REQUIRE (result.maxDegree() == 6);
+        REQUIRE (result[0] == Approx(4.00));
+        REQUIRE (result % 0 == 6);
+        REQUIRE (result[1] == Approx(-20.00));
+        REQUIRE (result % 1 == 4);
+        REQUIRE (result[2] == Approx(28.00));
+        REQUIRE (result % 2 == 3);
+        REQUIRE (result[3] == Approx(25.00));
+        REQUIRE (result % 3 == 2);
+        REQUIRE (result[4] == Approx(-70.00));
+        REQUIRE (result % 4 == 1);
+        REQUIRE (result[5] == Approx(49.00));
+        REQUIRE (result % 5 == 0);
+    }
+
+    SECTION("Multiplication of two polynoms") {
+        result = one * two;
+        REQUIRE (result.maxDegree() == 8);
+    /* 2x^3 - 5x + 7 */
+    /* 6x^5 + x^2 */
+        REQUIRE (result[0] == Approx(12.0));
+        REQUIRE (result % 0 == 8);
+        REQUIRE (result[1] == Approx(-30.0));
+        REQUIRE (result % 1 == 6);
+        REQUIRE (result[2] == Approx(44.0));
+        REQUIRE (result % 2 == 5);
+        REQUIRE (result[3] == Approx(-5.0));
+        REQUIRE (result % 3 == 3);
+        REQUIRE (result[4] == Approx(7.0));
+        REQUIRE (result % 4 == 2);
+    }
+
+    SECTION ("Multiplication is commutative") {
+        Polynomial<float> r1, r2;
+        r1 = one * two;
+        r2 = two * one;
+        for (int k = 0; k < 6; k++)
+            REQUIRE (r1[k] == Approx (r2[k]));
+    }
+
+    SECTION ("Compare two techniques") {
+        Polynomial<float> r1, r2;
+        r1 = one * two;
+        r2 = one % two;
+        for (int k = 0; k < 6; k++) {
+            REQUIRE (r1[k] == Approx (r2[k]));
+            REQUIRE (r1 % k == r2 % k);
+        }
+    }
+}
+
+TEST_CASE ("Large Polynomials") {
+    const int N1 = 5000, N2 = 2000;
+    ostringstream t1, t2;
+    for (int k = N1; k >= 0; k--)
+        t1 << "[1 " << k << "]";
+    for (int k = N2; k >= 0; k--)
+        t2 << "[1 " << k << "]";
+    Polynomial<float> p1(t1.str());
+    Polynomial<float> p2(t2.str());
+    Polynomial<float> prod1, prod2;
+    prod1 = p1 * p2;
+    for (int k = 0; k < N2; k++) {
+        REQUIRE (prod1[k] == k + 1);
+    }
+    for (int k = N2; k < N1; k++) {
+        REQUIRE (prod1[k] == Approx(N2+1));
+    }
+}
